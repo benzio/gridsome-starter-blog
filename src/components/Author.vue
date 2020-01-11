dd60449ad42abb187526030df916d7dbcde3dd10
benzio <16840399+benzio@users.noreply.github.com>
2020-01-11 08:27:29 -0800

    Update Author.vue

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -11,10 +11,7 @@
 			Blog
 		</p>
 
-		<p class="author__links">
-			<a href="//twitter.com/gridsome">Follow on Twitter</a>
-			<a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a>
-		</p>
+		
 
 	</div>
 </template>
