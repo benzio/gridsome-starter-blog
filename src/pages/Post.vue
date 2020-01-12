c9fd8046082fa4a7a6b00194bb558c106614e68f
benzio <16840399+benzio@users.noreply.github.com>
2020-01-12 14:10:08 -0800

    Update Post.vue

--- src/pages/Post.vue
+++ src/pages/Post.vue
@@ -1,58 +1,23 @@
 <template>
   <Layout>
-    <!--Hero-->
-    <section class="inner-hero">
-      <div class="container">
-        <div class="row text-center">
-          <div class="col-md-12">
-            <h1>Blog</h1>
-            <p>Our latest posts educating clients</p>
-          </div>
-        </div>
-      </div>
-    </section>
-    <!--Content-->
-    <section class="inner-content">
-      <div class="container">
-        <div class="row">
-          <!--Content-->
-          <div
-            v-for="edge in $page.posts.edges"
-            :key="edge.node.id"
-            class="col-md-12 text-center"
-          >
-            <h2>
-              <g-link :to="edge.node.path">{{ edge.node.title }}</g-link>
-            </h2>
-            <p>{{ edge.node.excerpt }}</p>
-          </div>
-        </div>
-      </div>
-    </section>
-    <Footer></Footer>
+    <h2>Latest blog posts</h2>
+    <ul>
+      <li v-for="edge in $page.allWordPressPost.edges" :key="edge.node.id">
+        {{ edge.node.title }}
+      </li>
+    </ul>
   </Layout>
 </template>
 
 <page-query>
-query BlogPosts {
-  posts: allPost {
+query Post {
+  allWordPressPost (limit: 5) {
     edges {
       node {
-        id
+        _id
         title
-        path
-        date (format: "D. MMMM YYYY")
-        excerpt
       }
     }
   }
 }
 </page-query>
-
-<script>
-export default {
-  metaInfo: {
-    title: 'Blog'
-  }
-}
-</script>
