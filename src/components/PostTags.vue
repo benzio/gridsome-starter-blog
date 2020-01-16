4665f2497db7ddb57552b1837339f44bd44191de
benzio <16840399+benzio@users.noreply.github.com>
2020-01-15 21:43:37 -0800

    tags.name

--- src/components/PostTags.vue
+++ src/components/PostTags.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="post-tags">
    		<g-link class="post-tags__link" v-for="tag in post.tags" :key="tag.id" :to="tag.path">
-   			<span>#</span> {{ tags.title }}
+   			<span>#</span> {{ tags.name }}
    		</g-link>
     </div>
 </template>
